9cef17a6984b9143c79e6d886f21a08e29cd629c
DogonYu <dorogon01@gmail.com>
2021-03-29 23:52:18 +0900

    setting: eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  quoteProps: 'preserve',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+};
